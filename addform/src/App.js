ad6c7ed1c9be773aef431eba169d2153ca6fefeb
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-04-27 15:00:37 +0300

    creating the form

--- addform/src/App.js
+++ addform/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+ 
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
-    <div>App</div>
+   <Form/>
   )
 }
 
---
+++ addform/src/components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+       <form>
+        <input type="number" id='number' placeholder='digits'/>
+        <input type="number" id='number' placeholder='digits'/>
+       </form>
+    </div>
+  )
+}
+
+export default Form
