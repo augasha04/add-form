b90d6293c1bf92e65ee8f009902877fbce4a9df2
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-04-28 13:18:32 +0300

    done with the assignment

--- addform/src/components/Form.js
+++ addform/src/components/Form.js
@@ -4,7 +4,7 @@ function Form() {
 
   const [num1, setNum1]=useState();
   const [num2, setNum2]=useState();
-  const [toContainHTML, setTotal]=useState(num1 + num2);
+  const [total, setTotal]=useState(num1 + num2);
 
   function addTotal(){
     setTotal(num1 + num2)
@@ -13,12 +13,10 @@ function Form() {
     <div>
       <form>
         <input type="number" id='number' placeholder='fisrtnum' value={num1} onChange={e=>setNum1(+e.target.value)}/>
-        <br/>
-        <p>After entering the digits hear, they should add fter clicking 
-          the button and display the results after</p>
         <input type="number" id='number' placeholder='secondnum' value={num2} onChange={e=>setNum2(+e.target.value)}/>
         <button onClick={addTotal}>add numbers</button>
         </form>
+        <h1>{total}</h1>
     </div>
   )
 }
