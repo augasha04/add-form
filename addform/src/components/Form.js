42ef0f7dd780fb407bdc6afbc4d810a5ed70a70c
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-04-28 12:44:49 +0300

    implementing on the placeholders well

--- addform/src/components/Form.js
+++ addform/src/components/Form.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form() {
+
+  const [num1, setNum1]=useState();
+  const [num2, setNum2]=useState();
+  cont [toContainHTML, setTotal]=useState(num1,num2);
+
+  function addTotal(){
+    setTotal(num1 + num2)
+  }
   return (
     <div>
        <form>
-        <input type="number" id='number' placeholder='digits'/>
+        <input type="number" id='number' placeholder='fisrtnum' value={num1} onChange={e=>setNum1(+e.target.value)}/>
         <br/>
-        <p>After entering the digits hear, they should add fter clicking the button and display the results after</p>
-        <input type="number" id='number' placeholder='digits'/>
+        <p>After entering the digits hear, they should add fter clicking 
+          the button and display the results after</p>
+        <input type="number" id='number' placeholder='secondnum' value={num2} onChange={e=>setNum2(+e.target.value)}/>
        </form>
     </div>
   )
