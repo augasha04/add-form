3b6809c0dc05a520e76f4321d289f9d8e65d7444
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-04-28 12:51:34 +0300

    implementing the forms

--- addform/src/App.js
+++ addform/src/App.js
@@ -4,7 +4,7 @@ import Form from './components/Form';
 
 function App() {
   return (
-   <Form />
+   <addTotal/>
   )
 }
 
--- addform/src/components/Form.js
+++ addform/src/components/Form.js
@@ -4,20 +4,21 @@ function Form() {
 
   const [num1, setNum1]=useState();
   const [num2, setNum2]=useState();
-  cont [toContainHTML, setTotal]=useState(num1,num2);
+  const [toContainHTML, setTotal]=useState(num1 + num2);
 
   function addTotal(){
     setTotal(num1 + num2)
   }
   return (
     <div>
-       <form>
+      <form>
         <input type="number" id='number' placeholder='fisrtnum' value={num1} onChange={e=>setNum1(+e.target.value)}/>
         <br/>
         <p>After entering the digits hear, they should add fter clicking 
           the button and display the results after</p>
         <input type="number" id='number' placeholder='secondnum' value={num2} onChange={e=>setNum2(+e.target.value)}/>
-       </form>
+        <button onClick={addTotal}>add numbers</button>
+        </form>
     </div>
   )
 }
